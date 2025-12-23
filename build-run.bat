a5063b98ffc85f8671df075ad6c5d868fcdb82b8
astralkingdummy <cjansah11@gmail.com>
2025-12-22 22:38:23 -0500

    fix batch script

--- build-run.bat
+++ build-run.bat
@@ -1,6 +1,6 @@
 @echo off
-cd /d "%~dp0" :: the script directory
-rmdir /s /q :: recursive deletion, quiet (so there's no freaking stupid confirmiation)
+cd /d "%~dp0"               :: the script directory
+rmdir /s /q build           :: recursive deletion, quiet (so there's no freaking stupid confirmiation)
 cmake -B build
 cmake --build build
 build\Debug\fluid_sim.exe
