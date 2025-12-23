33bbc58793520a0a142a74c9af7a970b1428d54e
astralkingdummy <cjansah11@gmail.com>
2025-12-22 22:22:42 -0500

    a little love

--- build-run-mac.sh
+++ build-run-mac.sh
@@ -1,4 +1,4 @@
-# for the mac aka linux professionals
+# for the mac aka linux professionals (arsenii is wicked)
 # before you run this remember to call chmod +x build-run-mac.sh to permit execution
 # and to run it from the terminal with ./build-run-mac.sh
 
