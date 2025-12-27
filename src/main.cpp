4c96be984289e6e296535fb8bec00e2c175af9a8
astralkingdummy <cjansah11@gmail.com>
2025-12-27 16:13:09 -0500

    new structure and add constants header

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,54 @@
+#define TIME_SCALE 60.0
+
 #include "raylib.h"
+#include "constants.h"
+
+static void InitSim(void); // initialize the simulation
+static void UpdateSim(void); // update a single frame of simulation
+static void DrawSim(void); // draw the visuals on the frame
+static void UnloadSim(void); // honestly might not need this
+static void UpdateDrawSim(void); // update & draw
+
+const float acceleration = GRAVITY;
 
 int main() {
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "arsenii likes balls");
+    InitSim();
 
-    const int screenWidth = 1280;
-    const int screenHeight = 720;
-    const float acceleration = 9.8; 
-
-    Vector2 pos = {screenWidth/2.0, 25}; 
-    Vector2 vel = {0.0, 0.0};
-    Color pink = {255, 144, 202, 255};
-    Color teal = {40, 225, 237, 255};
-
-    InitWindow(screenWidth, screenHeight, "Move-a-ball");
     SetTargetFPS(60);
+    
 
     while (!WindowShouldClose()) {
-
-        //unforced movement (velocity and acceleration)
-        pos.x += vel.x;
-        pos.y += vel.y;
-        vel.y += acceleration/60.0; //per frame
-        vel.x += 5.0f/60.0f; //artificial sideways movement
-
-        //forced movement
-        if(IsKeyDown(KEY_RIGHT)){
-            pos.x += 2.0f;
-        };
-        if(IsKeyDown(KEY_LEFT)){
-            pos.x -= 2.0f;
-        };
-        if(IsKeyDown(KEY_UP)){
-            pos.y -= 2.0f;
-        };
-        if(IsKeyDown(KEY_DOWN)){
-            pos.y += 2.0f;
-        };
-
-        //boundary collision
-        if(pos.y >= screenHeight - 20 || pos.y <= 20){
-            vel.y *= -0.95;
-        };
-        if(pos.x <= 20 || pos.x >= screenWidth -20){
-            vel.x *= -0.95;
-        };
-
-        //things appearing on the screen
-        BeginDrawing();
-
-        ClearBackground(pink);
-        
-        DrawText("Use arrows to move the ball", 10, 10, 30, BLACK);
-
-        DrawCircleV(pos, 20, teal);
-        
-        EndDrawing();
+        UpdateDrawSim();
     }
-
+    
+    UnloadSim();
     CloseWindow(); //esc or cross button trigger
     return 0;
+}
+
+void InitSim(void) {
+
+}
+
+void UpdateSim() {
+    //lol
+}
+
+void DrawSim() {
+    BeginDrawing();
+
+    ClearBackground(PINK);    
+    DrawText("Use arrows to move the ball", 10, 10, 30, BLACK);
+    
+    EndDrawing();
+}
+
+void UnloadSim() {
+
+}
+
+void UpdateDrawSim() {
+    UpdateSim();
+    DrawSim();
 }
